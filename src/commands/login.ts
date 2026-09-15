0477458f7243e04991f3f4fe5e41e3b864564f32
adm22222 <admakrm18@gmail.com>
2026-09-15 23:01:59 +0300

    feat: implement command registration and login handler

---
+++ src/commands/login.ts
@@ -0,0 +1,12 @@
+import { setUser } from "../config.js";
+
+export const handlerLogin = (cmdName: string, ...args: string[]) => {
+
+    if (args.length !== 1) {
+        throw new Error(`usage: ${cmdName} <name>`);
+    }
+
+    const username = args[0];
+    setUser(username);
+    console.log(`User ${username} logged in`);
+} 
