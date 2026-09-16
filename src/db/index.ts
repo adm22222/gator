133d62aecc648784fb00b50a6e59da56419ddf8f
adm22222 <admakrm18@gmail.com>
2026-09-16 21:33:39 +0300

    feat: add database connection setup with drizzle ORM

---
+++ src/db/index.ts
@@ -0,0 +1,10 @@
+import postgres from "postgres";
+import { drizzle } from "drizzle-orm/postgres-js";
+
+import { readConfig } from "../config.js";
+import * as schema from "./schema.js";
+
+const config = readConfig();
+const conn = postgres(config.dbUrl);
+
+export const db = drizzle(conn, { schema });
