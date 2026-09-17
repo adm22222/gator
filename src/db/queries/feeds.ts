5663498385434d062e6a9f26b07a25fe23903a4f
adm22222 <admakrm18@gmail.com>
2026-09-17 16:22:05 +0300

    fix: update feed references in createFeed and getAllFeeds functions

--- src/db/queries/feeds.ts
+++ src/db/queries/feeds.ts
@@ -1,20 +1,20 @@
 import { eq } from "drizzle-orm";
 import { db } from "../index.js"
-import { feed, users } from "../schema.js"
+import { feeds, feedFollows, users } from "../schema.js"
 
 
 export const createFeed = async (name: string, url: string, userId: string) => {
-    const [feedData] = await db.insert(feed).values({ name, url, userId }).returning();
+    const [feedData] = await db.insert(feeds).values({ name, url, userId }).returning();
     return feedData;
 }
 
 export const getAllFeeds = async () => {
     const feedsData = await db.select({
-        id: feed.id,
-        name: feed.name,
-        url: feed.url,
-        userId: feed.userId,
+        id: feeds.id,
+        name: feeds.name,
+        url: feeds.url,
+        userId: feeds.userId,
         userName: users.name,
-    }).from(feed).innerJoin(users, eq(feed.userId, users.id)).execute();
+    }).from(feeds).innerJoin(users, eq(feeds.userId, users.id)).execute();
     return feedsData;
 }
