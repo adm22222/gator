6e5cf45f2f2de13c604ad3acc6cd09975aed82cc
adm22222 <admakrm18@gmail.com>
2026-09-17 16:16:36 +0300

    feat: add feed follows table and update schema for feed management

---
+++ drizzle/0003_watery_grey_gargoyle.sql
@@ -0,0 +1,9 @@
+ALTER TABLE "feed" RENAME TO "feeds";--> statement-breakpoint
+ALTER TABLE "feeds" DROP CONSTRAINT "feed_url_unique";--> statement-breakpoint
+ALTER TABLE "feeds" DROP CONSTRAINT "feed_user_id_users_id_fk";
+--> statement-breakpoint
+ALTER TABLE "feed_follows" DROP CONSTRAINT "feed_follows_feed_id_feed_id_fk";
+--> statement-breakpoint
+ALTER TABLE "feeds" ADD CONSTRAINT "feeds_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "feed_follows" ADD CONSTRAINT "feed_follows_feed_id_feeds_id_fk" FOREIGN KEY ("feed_id") REFERENCES "public"."feeds"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "feeds" ADD CONSTRAINT "feeds_url_unique" UNIQUE("url");
